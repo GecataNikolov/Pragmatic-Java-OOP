4160b630704c8e334ec1d0665285d217161dde48
Georgi <Raiden.kinkoid@gmail.com>
2019-05-23 16:00:49 +0300

    Adding the class, return method, void method, constructor and properties

---
+++ src/Arrays/HW8_ForTable.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class HW8_ForTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number:");
+        int number = sc.nextInt();
+        int printNumber = number - 1;
+
+        for (int i = 0; i < number ; i++) {
+            System.out.print(printNumber);
+            for (int j = 1; j < number ; j++) {
+                System.out.print(printNumber);
+
+            }
+            printNumber+=2;
+            System.out.println();
+        }
+    }
+}
